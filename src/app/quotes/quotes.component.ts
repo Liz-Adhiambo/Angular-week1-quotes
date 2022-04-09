fea19eb1f4dd401a7c15598e74b5228c1fd739ef
Liz <adhiamboliz3@gmail.com>
2022-04-09 23:48:10 +0300

    Added constructor in class quote and a quotes array in quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes:Quotes[]=[
+    new Quotes('Sample quote 1','~by me', new Date(2022,4,9)),
+    new Quotes('sample quote 2','~by me', new Date(2022,4,9)),
+    new Quotes('sample quote 3','~by me', new Date(2022,4,9)),
+    
+]
 
   constructor() { }
 
